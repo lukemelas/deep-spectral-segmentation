e5895a9f6108751fd30ccf730a1111bec95b08bd
Luke <luke.melas@gmail.com>
2021-10-29 12:26:39 -0400

    Working

--- preprocess/extract-features/extract-segments.py
+++ preprocess/extract-features/extract-segments.py
@@ -59,6 +59,12 @@ def _create_object_segment(
     k_feats = data_dict['k'].squeeze()  # CPU
     img_np = np.array(_inverse_transform(data_dict['images_resized'].squeeze(0)))
 
+    # Upscale features
+    features = F.interpolate(
+        data_dict['out'][0][:, 1:].permute(2, 0, 1).reshape(1, -1, H_patch, W_patch), 
+        size=(H_pad, W_pad), mode='bilinear', align_corners=False
+    ).squeeze()
+
     # # Eigenvectors of affinity matrix
     # A = k_feats @ k_feats.T
     # eigenvalues, eigenvectors = torch.eig(A, eigenvectors=True)
@@ -82,39 +88,53 @@ def _create_object_segment(
     new_data_dict = defaultdict(list)
     for k in range(K):
         eigenvector = eigenvectors[:, k]
-        ############# Segments ############
 
+        ############# Segments ############
+        eigensegment = eigenvector.clone()
+        if 0.5 < torch.mean((eigensegment > threshold).float()).item() < 1.0:  # reverse segment
+            eigensegment = 0 - eigensegment
+        # Do CRF on high-resolution features
+        U = F.interpolate(
+            eigensegment.reshape(1, 1, H_patch, W_patch), 
+            size=(H_pad, W_pad), mode='bilinear', align_corners=False
+        ).squeeze()
+        U = (U - U.min()) / (U.max() - U.min())
+        U = torch.stack((1 - U, U), dim=-1)
+        eigensegment = denseCRF.densecrf(img_np, U.numpy(), crf_params)
+        eigensegment = torch.from_numpy(eigensegment)
+        # Get feature
+        pooled_feature = (eigensegment.unsqueeze(0) * features).mean(dim=(1,2))
 
         ############# Objects #############
         # Get segment
         object_segment = (eigenvector > threshold).float()
         if 0.5 < torch.mean(object_segment).item() < 1.0:  # reverse segment
             object_segment = (1 - object_segment)
-        # Do CRF
-        unary_potentials = F.interpolate(
+        # Do CRF with unary potentials U
+        U = F.interpolate(
             object_segment.reshape(1, 1, H_patch, W_patch), 
-            size=(H_pad, W_pad), mode='bilinear'
+            size=(H_pad, W_pad), mode='bilinear', align_corners=False
         ).squeeze()
-        unary_potentials = torch.stack((1 - unary_potentials, unary_potentials), dim=-1)
-        object_segment = denseCRF.densecrf(img_np, unary_potentials.numpy(), crf_params)
+        U = torch.stack((1 - U, U), dim=-1)
+        object_segment = denseCRF.densecrf(img_np, U.numpy(), crf_params)
+        # In case there is no object
+        if np.sum(np.abs(object_segment)) == 0:
+            object_segment = 1 - object_segment
         # Get largest connected component
         object_segment = get_largest_cc(object_segment)
         object_segment = torch.from_numpy(object_segment)
         # Get and pool features
-        object_pooled_feature = F.interpolate(
-            data_dict['out'][0][:, 1:].permute(2, 0, 1).reshape(1, -1, H_patch, W_patch), 
-            size=(H_pad, W_pad), mode='bilinear'
-        ).squeeze()
-        object_pooled_feature = (object_segment.unsqueeze(0) * object_pooled_feature).mean(dim=(1,2))
-        # Add to dict
+        object_pooled_feature = (object_segment.unsqueeze(0) * features).mean(dim=(1,2))
+
+        ############# Output #############
         new_data_dict['eigenvectors'].append(eigenvector)
-        # new_data_dict['eigensegments'].append(eigensegment)
-        # new_data_dict['pooled_features'].append(pooled_feature)
+        new_data_dict['eigensegments'].append(eigensegment)
+        new_data_dict['pooled_features'].append(pooled_feature)
         new_data_dict['eigensegments_object'].append(object_segment)
         new_data_dict['pooled_features_object'].append(object_pooled_feature)
     new_data_dict['eigenvectors'] = torch.stack(new_data_dict['eigenvectors'])
-    # new_data_dict['eigensegments'] = torch.stack(new_data_dict['eigensegments'])
-    # new_data_dict['pooled_features'] = torch.stack(new_data_dict['pooled_features'])
+    new_data_dict['eigensegments'] = torch.stack(new_data_dict['eigensegments'])
+    new_data_dict['pooled_features'] = torch.stack(new_data_dict['pooled_features'])
     new_data_dict['eigensegments_object'] = torch.stack(new_data_dict['eigensegments_object'])
     new_data_dict['pooled_features_object'] = torch.stack(new_data_dict['pooled_features_object'])
     new_data_dict = dict(new_data_dict)
@@ -124,114 +144,33 @@ def _create_object_segment(
 
 
 @torch.no_grad()
-def create_object_segments(
+def create_segments(
     prefix: str,
     features_root: str = './features',
     output_dir: str = './object_eigensegments',
     K: int = 3, 
     threshold: float = 0.0, 
-    multiprocessing: bool = False
-):
-    """
-    Example:
-    python extract-segments.py create_object_segments \
-        --prefix VOC2007-dino_vits16 \
-        --features_root ./features \
-        --output_dir ./object_eigensegments \
-    """
-    start = time.time()
-    _create_object_segment_fn = partial(_create_object_segment, K=K, threshold=threshold, crf_params=CRF_PARAMS, prefix=prefix, output_dir=output_dir)
-    inputs = list(enumerate(sorted(Path(features_root).iterdir())))  # inputs are (index, files) tuples
-    if multiprocessing:
-        from multiprocessing import Pool
-        with Pool() as pool:
-            list(tqdm(pool.imap(_create_object_segment_fn, inputs), total=len(inputs)))
-    else:
-        for inp in tqdm(inputs):
-            _create_object_segment_fn(inp)
-    print(f'Done in {time.time() - start:.1f}s')
-
-
-@torch.no_grad()
-def create_segments(
-    prefix: str,
-    features_root: str = './features',
-    output_dir: str = './eigensegments',
-    K: int = 5
+    multiprocessing: int = 0
 ):
     """
     Example:
     python extract-segments.py create_segments \
         --prefix VOC2007-dino_vits16 \
         --features_root ./features \
-        --output_dir ./eigensegments \
+        --output_dir ./object_eigensegments \
     """
-
-    # Params
-    device = 'cuda'
-    ParamsCRF = namedtuple('ParamsCRF', 'w1 alpha beta w2 gamma it')
-    crf_params = ParamsCRF(
-        w1    = 10.0,  # weight of bilateral term
-        alpha = 80,    # spatial std
-        beta  = 13,    # rgb  std
-        w2    = 3.0,   # weight of spatial term
-        gamma = 3,     # spatial std
-        it    = 5.0,   # iteration
-    )
-
-    # Load
-    for i, p in tqdm(list(enumerate(sorted(Path(features_root).iterdir())))):
-        output_dict = torch.load(p, map_location='cpu')
-
-        # Sizes
-        P = 16
-        B, C, H, W = output_dict['shape']
-        assert B == 1, 'assumption violated :('
-        H_patch, W_patch = H // P, W // P
-        H_pad, W_pad = H_patch * P, W_patch * P
-        k_feats = output_dict['k'].squeeze().to(device)
-        img_np = np.array(_inverse_transform(output_dict['images_resized'].squeeze(0)))
-
-        # Eigenvectors of affinity matrix
-        A = k_feats @ k_feats.T
-        eigenvalues, eigenvectors = torch.eig(A, eigenvectors=True)
-        
-        # # Eigenvectors of affinity matrix with scipy
-        # from scipy.sparse.linalg import eigsh
-        # A = k_feats @ k_feats.T
-        # eigenvalues, eigenvectors = eigsh(A.cpu().numpy(), which='LM', k=K)  # find small eigenvalues
-        # eigenvectors = torch.from_numpy(eigenvectors)
-        
-        # # Eigenvectors of laplacian matrix
-        # from scipy.sparse.csgraph import laplacian
-        # from scipy.sparse.linalg import eigsh
-        # A = (k_feats @ k_feats.T).cpu().numpy()
-        # L = laplacian(A, normed=False)
-        # eigenvalues, eigenvectors = eigsh(L, sigma=0, which='LM', k=K)  # find small eigenvalues
-        # eigenvectors = torch.from_numpy(eigenvectors)
-        # print(eigenvectors.shape)
-
-        # CRF
-        new_output_dict = {'eigensegments': [], 'pooled_features': []}
-        for k in range(K):
-            # Get potentials
-            # unary_potentials = F.interpolate(eigenvectors[:, k].reshape(1, 1, H_patch, W_patch), size=(H, W), mode='bilinear').squeeze()  # IS THIS THE PROBLEM ????? YES!
-            unary_potentials = F.interpolate(eigenvectors[:, k].reshape(1, 1, H_patch, W_patch), size=(H_pad, W_pad), mode='bilinear').squeeze()
-            unary_potentials = (unary_potentials - unary_potentials.min()) / (unary_potentials.max() - unary_potentials.min())
-            unary_potentials_np = torch.stack((1 - unary_potentials, unary_potentials), dim=-1).detach().cpu().numpy()
-            # Do CRF
-            eigensegment = denseCRF.densecrf(img_np, unary_potentials_np, crf_params)
-            # Get and pool features
-            pooled_feature = F.interpolate(output_dict['out'][0][:, 1:].permute(2, 0, 1).reshape(1, -1, H_patch, W_patch), size=(H_pad, W_pad), mode='bilinear').squeeze()
-            pooled_feature = (torch.from_numpy(eigensegment[None]) * pooled_feature).mean(dim=(1,2))
-            # Add to dict
-            new_output_dict['eigensegments'].append(torch.from_numpy(eigensegment))
-            new_output_dict['pooled_features'].append(pooled_feature)
-        new_output_dict['eigensegments'] = torch.stack(new_output_dict['eigensegments'])  # [::-1])
-        new_output_dict['pooled_features'] = torch.stack(new_output_dict['pooled_features'])  # [::-1])
-        # Save dict
-        output_file = str(Path(output_dir) / f'{prefix}-eigensegments-{i:05d}.pth')
-        torch.save(new_output_dict, output_file)
+    start = time.time()
+    _create_object_segment_fn = partial(_create_object_segment, K=K, threshold=threshold, crf_params=CRF_PARAMS,
+                                        prefix=prefix, output_dir=output_dir)
+    inputs = list(enumerate(sorted(Path(features_root).iterdir())))  # inputs are (index, files) tuples
+    if multiprocessing:
+        from multiprocessing import Pool
+        with Pool(multiprocessing) as pool:
+            list(tqdm(pool.imap(_create_object_segment_fn, inputs), total=len(inputs)))
+    else:
+        for inp in tqdm(inputs):
+            _create_object_segment_fn(inp)
+    print(f'Done in {time.time() - start:.1f}s')
 
 
 def combine_segments(
@@ -299,7 +238,6 @@ def vis_segments(
 
 if __name__ == '__main__':
     fire.Fire(dict(
-        create_object_segments=create_object_segments, 
         create_segments=create_segments, 
         combine_segments=combine_segments, 
         vis_segments=vis_segments
